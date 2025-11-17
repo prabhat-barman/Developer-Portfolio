73a674232e6d03148f06257999e8caa5b624526d
pbarman_sxm <Prabhat.barman@siriusxm.com>
2025-11-17 17:23:40 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Developer-Portfolio/",
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  publicDir: "public",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
+    assetsDir: "assets",
   },
-  server: {
-    port: 3000,
-    open: true,
-  },
-})
+});
