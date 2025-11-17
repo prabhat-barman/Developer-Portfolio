3aa7fafa50623c6a8de6249eb92c42de57b7c3bc
pbarman_sxm <Prabhat.barman@siriusxm.com>
2025-11-17 16:50:05 +0530

    update all file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+     base: "/Developer-Portfolio/", // IMPORTANT
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
